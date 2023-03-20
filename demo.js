e26aa0ed69aec50cf83212da28c557af6908d6f1
KMihovic <120470308+KMihovic@users.noreply.github.com>
2023-03-20 14:28:51 +0100

    rev_07_Info_Popup_Done

--- demo.js
+++ demo.js
@@ -26,13 +26,13 @@ var ui = H.ui.UI.createDefault(map, defaultLayers);
 var LocationOfMarker = { lat: 45.327980, lng: 14.476690 };
 
 // Create a marker icon from an image URL:
-var pngIcon = new H.map.Icon("https://cdn2.iconfinder.com/data/icons/business-development-6/24/Aircraft_transport_plane_transportation_airplane_travel-512.png", { size: { w: 40, h: 40 } });
+var pngIcon = new H.map.Icon("https://cdn2.iconfinder.com/data/icons/business-development-6/24/Aircraft_transport_plane_transportation_airplane_travel-512.png", { size: { w: 50, h: 50 } });
 
 // Create a marker using the previously instantiated icon:
 var marker = new H.map.Marker(LocationOfMarker, { icon: pngIcon });
 
 // Add the marker to the map:
-map.addObject(marker);
+// map.addObject(marker);
 
 // Now use the map as required...
 window.onload = function () {
@@ -48,27 +48,115 @@ const url = `https://opensky-network.org/api/states/all?lamin=44&lomin=9.5&lamax
 fetch(url)
   .then(response => response.json())
   .then(data => {
-
     // Parse flight data
     const flights = data.states;
     
+    for (i = 0; i < flights.length; i++) {
+      const flight = flights[i];
+      // Extract flight information
+      const flightId = flight[0];
+      const origin_country = flight[2];
+      const longitude = flight[5];
+      const latitude = flight[6];
+      const baro_altitude = flight[7];
+      const on_ground = flight[8];
+      const velocity = flight[9];
+      const true_track = flight[10];
+      const geo_altitude = flight[13];
+
+      // Create a marker icon from an image URL:
+      var pngIcon = new H.map.Icon("https://cdn2.iconfinder.com/data/icons/business-development-6/24/Aircraft_transport_plane_transportation_airplane_travel-512.png", { size: { w: 30, h: 30 } });
+
+      function addMarkerToGroup(group, coordinate, html) {
+        const marker = new H.map.Marker(coordinate, { icon: pngIcon });
+        marker.setData(html);
+        group.addObject(marker);
+        //map.addObject(marker);
+      }
+      function addInfoBubble(map) {
+        var group = new H.map.Group();
+    
+        map.addObject(group);
+        // add 'tap' event listener, that opens info bubble, to the group
+        group.addEventListener('tap', function (evt) {
+          // event target is the marker itself, group is a parent event target
+          // for all objects that it contains
+          var bubble = new H.ui.InfoBubble(evt.target.getGeometry(), {
+            // read custom data
+            content: evt.target.getData()
+          });
+          // show info bubble
+          ui.addBubble(bubble);
+        }, false);
+      
+        addMarkerToGroup(group, {lat:latitude, lng:longitude},
+          '<b>Flight ID:</b>' + flightId +
+          '<br><b>Country:</b>' + origin_country +
+          '<br><b>Barometric_Altitude</b>:' + baro_altitude +
+          '<br><b>On_Ground:</b>' + on_ground +
+          '<br><b>Velocity:</b>' + velocity + 'm/s' + 
+          '<br><b>True_Track:</b>' + true_track +
+          '<br><b>Geo_Altitude:</b>' + geo_altitude);
+      }
+      addInfoBubble(map);
+    }
+    });  
+
+
+/*
+//ZA PRIKAZ SAMO JEDNOG RANDOM AVIONA
+// ADDING DATA FROM OPENSKY PLATFORM ------------------------------------------
+// Retrieve flight data for Croatia
+const url = `https://opensky-network.org/api/states/all?lamin=44&lomin=9.5&lamax=46.5&lomax=19.4`;
+
+// Retrieve flight data using fetch
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // Parse flight data
+    const flights = data.states;
     // Select a random flight
     const randomIndex = Math.floor(Math.random() * flights.length);
     const flight = flights[randomIndex];
-    
     // Extract flight information
-    const flightId = flight[0];     //Now we dont use but later we will use it for flight ID
     const latitude = flight[6];
     const longitude = flight[5];
- 
+    const flightId = flight[0];
+    const baro_altitude = flight[7];
     // Create a marker icon from an image URL:
     var pngIcon = new H.map.Icon("https://cdn2.iconfinder.com/data/icons/business-development-6/24/Aircraft_transport_plane_transportation_airplane_travel-512.png", { size: { w: 40, h: 40 } });
+    
+    function addMarkerToGroup(group, coordinate, html) {
+      const marker = new H.map.Marker(coordinate, { icon: pngIcon });
+      marker.setData(html);
+      group.addObject(marker);
+      //map.addObject(marker);
+    }
+    function addInfoBubble(map) {
+      var group = new H.map.Group();
+    
+      map.addObject(group);
+      // add 'tap' event listener, that opens info bubble, to the group
+      group.addEventListener('tap', function (evt) {
+        // event target is the marker itself, group is a parent event target
+        // for all objects that it contains
+        var bubble = new H.ui.InfoBubble(evt.target.getGeometry(), {
+          // read custom data
+          content: evt.target.getData()
+        });
+        // show info bubble
+        ui.addBubble(bubble);
+      }, false);
+    
+      addMarkerToGroup(group, {lat:latitude, lng:longitude},
+        '<div><a href="https://www.mcfc.co.uk">Manchester City</a></div>' +
+        '<div>City of Manchester Stadium<br />Capacity: 55,097</div>');
+    }
+    addInfoBubble(map);
+  });    
+*/
 
-    const marker = new H.map.Marker({lat: latitude, lng: longitude}, { icon: pngIcon });
-    map.addObject(marker);
-  });
-
-
+  
 // ADDING & REMOVING 20 RANDOM AIRPLANES --------------------------------------
 // Function to generate random number
 function randomLat() {
